b809fc4854cc763eec0dd3fe75ce8d7b4911c53e
mehedi hasan <mehedihasan500362@gmail.com>
2024-08-19 18:30:57 +0900

    Rectengel area commit

---
+++ js/Rectengle.js
@@ -0,0 +1,18 @@
+ // Rectengle Area
+ function myRectengle(){
+    console.log("li")
+    const RectangleInputValue = document.getElementById("RectangleInput");
+    const RectangleInputString = RectangleInputValue.value;
+    const RectangleInputNumber = parseFloat(RectangleInputString);
+    RectangleInputValue.value = '';
+
+     const RectangleLineValue = document.getElementById("RectangleLine");
+    const RectangleLineString = RectangleLineValue.value;
+    const RectangleLineNumber = parseFloat(RectangleLineString);
+    RectangleLineValue.value = '';
+
+    const RectangleArea = RectangleInputNumber*RectangleLineNumber;
+
+    const setRectangleValue = document.getElementById("Rectanglevalue");
+    setRectangleValue.innerText = RectangleArea;
+ }
